7d4c3716705ae86f68e459ec5bc6bd652b2dc93d
whatshubhamdoes <shubhamprabhakar14@gmail.com>
2023-08-02 21:02:21 +0100

    lights tested. to do link with ui

--- rendererswapper.py
+++ rendererswapper.py
@@ -28,6 +28,45 @@ def assignNumber(currentRenderer):
     else:
         return False 
 
+def copyLightAttributes(light,lightsData,light_type,number):
+    try:
+        a_intensity = lightsData["Lights"][f"{light_type}"]["attribute_intensity"][number]
+        intensity=cmds.getAttr(f"{light}.{a_intensity}")
+    except:
+        pass
+    try:
+        a_color = lightsData["Lights"][f"{light_type}"]["attribute_color"][number]
+        l_color = cmds.getAttr(f"{light}.{a_color}")
+    except:
+        pass
+    try:
+        a_exposure = lightsData["Lights"][f"{light_type}"]["attribute_exposure"][number]
+        exposure = cmds.getAttr(f"{light}.{a_exposure}")
+    except:
+        pass
+    convLight=lightsData["Lights"][light_type]["name"][2] #change 1 to a variable as per UI
+    print(convLight)
+    convLight=cmds.shadingNode(f"{convLight}",asLight=True,name=f"{light}_conv")
+    try:
+        a_intensity = lightsData["Lights"][f"{light_type}"]["attribute_intensity"][2]
+        cmds.setAttr(f"{convLight}.{a_intensity}", intensity)
+    except:
+        pass
+    try:
+        a_color = lightsData["Lights"][f"{light_type}"]["attribute_color"][2]
+        cmds.setAttr(f"{convLight}.{a_color}", l_color[0][0], l_color[0][1], l_color[0][2], type="double3")
+    except:
+        pass
+    try:
+        a_exposure = lightsData["Lights"][f"{light_type}"]["attribute_exposure"][2] 
+        cmds.setAttr(f"{convLight}.{a_exposure}", exposure)
+    except:
+        pass
+    lightTransform=cmds.listRelatives(light,parent=True,shapes=True,fullPath=True)
+    cmds.matchTransform(f"{convLight}", lightTransform)
+    lightSet=cmds.listRelatives(lightTransform,parent=True,fullPath=True)
+    if lightSet:
+        cmds.parent(f"{convLight}", lightSet)    
 
 def lightsConversion():
     with open('Dictionary/lights.json','r') as file :
@@ -47,26 +86,32 @@ def lightsConversion():
             light_type = cmds.nodeType(light)
             print(light_type)
             if (light_type == lightsData["Lights"]["area_light"]["name"][number]):
-                a_intensity = lightsData["Lights"]["area_light"]["attribute_intensity"][number]
-                a_color = lightsData["Lights"]["area_light"]["attribute_color"][number]
-                a_exposure = lightsData["Lights"]["area_light"]["attribute_exposure"][number]
-                intensity=cmds.getAttr(f"{light}.{a_intensity}")
-                l_color = cmds.getAttr(f"{light}.{a_color}")
-                exposure = cmds.getAttr(f"{light}.{a_exposure}")
-                convLight=lightsData["Lights"]["area_light"]["name"][1] #change 1 to a variable as per UI
-                print(convLight)
-                convLight=cmds.shadingNode(f"{convLight}",asLight=True,name=f"{light}_conv")
-                a_intensity = lightsData["Lights"]["area_light"]["attribute_intensity"][1]
-                a_color = lightsData["Lights"]["area_light"]["attribute_color"][1]
-                a_exposure = lightsData["Lights"]["area_light"]["attribute_exposure"][1]
-                cmds.setAttr(f"{convLight}.{a_intensity}", intensity)
-                cmds.setAttr(f"{convLight}.{a_color}", l_color[0][0], l_color[0][1], l_color[0][2], type="double3")
-                cmds.setAttr(f"{convLight}.{a_exposure}", exposure)
-                lightTransform=cmds.listRelatives(light,parent=True,shapes=True,fullPath=True)
-                cmds.matchTransform(f"{convLight}", lightTransform)
-                lightSet=cmds.listRelatives(lightTransform,parent=True,fullPath=True)
-                if lightSet:
-                    cmds.parent(f"{convLight}", lightSet)
+                light_type="area_light"
+                print(light_type)
+                copyLightAttributes(light,lightsData,light_type,number)
+            elif(light_type == lightsData["Lights"]["point_light"]["name"][number]):
+                light_type="point_light"
+                print(light_type)
+                copyLightAttributes(light,lightsData,light_type,number)
+            elif(light_type == lightsData["Lights"]["directional_light"]["name"][number]):
+                light_type="directional_light"
+                print(light_type)
+                copyLightAttributes(light,lightsData,light_type,number)
+            elif(light_type == lightsData["Lights"]["spot_light"]["name"][number]):
+                light_type="spot_light"
+                print(light_type)
+                copyLightAttributes(light,lightsData,light_type,number)
+            elif(light_type == lightsData["Lights"]["skyDome_light"]["name"][number]):
+                light_type="skyDome_light"
+                print(light_type)
+                copyLightAttributes(light,lightsData,light_type,number)
+            elif(light_type == lightsData["Lights"]["mesh_light"]["name"][number]):
+                light_type="mesh_light"
+                print(light_type)
+                copyLightAttributes(light,lightsData,light_type,number)            
+            else:
+                pass
+
 
 def convertLights():
     currentRenderer=getCurrentRenderer()
