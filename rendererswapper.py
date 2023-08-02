673a23fed313d075d16bb8c1b8af90c9e80aa889
whatshubhamdoes <shubhamprabhakar14@gmail.com>
2023-08-02 14:32:35 +0100

    .

--- rendererswapper.py
+++ rendererswapper.py
@@ -14,16 +14,48 @@ def checkCurrentRenderer(currentRenderer):
     with open('Dictionary/lights.json','r') as file :
         lightsData=json.load(file)
     for rendererInfo in lightsData["Lights"]["renderer"]:
-        print(rendererInfo)
+        #print(rendererInfo)
         if currentRenderer in rendererInfo:
             return ("Renderer Supported")
+
+def assignNumber(currentRenderer):
+    if currentRenderer == "arnold" :
+        return 0
+    elif currentRenderer == "renderman" :
+        return 1
+    elif currentRenderer == "vray" :
+        return 2
+    else:
+        return False 
+
+def lightsConversion():
+    with open('Dictionary/lights.json','r') as file :
+        lightsData=json.load(file)
+
+    currentRenderer=getCurrentRenderer()    
+    number=assignNumber(currentRenderer)
+
+    sel = cmds.ls(sl=True)
+    if not sel :
+        cmds.confirmDialog(title='Error', message='Please select an object', button=['OK'], defaultButton='OK')
+        cmds.warning("Error : Please select an object")
+        return
+    for obj in sel:    
+        lights = cmds.ls(sl = True, dag = True, s = True)
+        print(lights)
+        for light in lights:
+            print(light)  
+            light_type = cmds.nodeType(light)
+            print(light_type)
+
+
 
 def convertLights():
     currentRenderer=getCurrentRenderer()
     print(currentRenderer)
     check1=checkCurrentRenderer(currentRenderer)
     if check1== "Renderer Supported":
-        print ("Okay")
+        lightsConversion()
     else:
         print("Renderer not supported for conversion")
 
