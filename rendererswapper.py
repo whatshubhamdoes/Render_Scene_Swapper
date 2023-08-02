77cad4827646bb7b18ad3520d8cad5ec074b8ea7
whatshubhamdoes <shubhamprabhakar14@gmail.com>
2023-08-02 14:09:12 +0100

    renderer checker added

--- rendererswapper.py
+++ rendererswapper.py
@@ -5,3 +5,26 @@ import maya.mel as mel
 import os
 import sys
 import json
+
+def getCurrentRenderer():
+    currentRenderer = cmds.getAttr('defaultRenderGlobals.currentRenderer')
+    return currentRenderer
+
+def checkCurrentRenderer(currentRenderer):
+    with open('Dictionary/lights.json','r') as file :
+        lightsData=json.load(file)
+    for rendererInfo in lightsData["Lights"]["renderer"]:
+        print(rendererInfo)
+        if currentRenderer in rendererInfo:
+            return ("Renderer Supported")
+
+def convertLights():
+    currentRenderer=getCurrentRenderer()
+    print(currentRenderer)
+    check1=checkCurrentRenderer(currentRenderer)
+    if check1== "Renderer Supported":
+        print ("Okay")
+    else:
+        print("Renderer not supported for conversion")
+
+convertLights()
