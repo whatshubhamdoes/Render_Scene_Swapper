df38be5ff930707bdf162a4cc39355e277c52c8d
whatshubhamdoes <shubhamprabhakar14@gmail.com>
2023-08-03 15:36:49 +0100

    clean and new material swapper in progress

--- new_materialswapper.py
+++ new_materialswapper.py
@@ -11,9 +11,9 @@ def getCurrentRenderer():
     return currentRenderer
 
 def checkCurrentRenderer(currentRenderer):
-    with open('/transfer/s5512613_SP/Masters_Project/Render_Scene_Swapper/Dictionary/materials.json','r') as file :
+    with open('/transfer/s5512613_SP/Masters_Project/Render_Scene_Swapper/Dictionary/lights.json','r') as file :
         lightsData=json.load(file)
-    for rendererInfo in lightsData["Materials"]["renderer"]:
+    for rendererInfo in lightsData["Lights"]["renderer"]:
         if currentRenderer in rendererInfo:
             return ("Renderer Supported")
 
@@ -27,49 +27,10 @@ def assignNumber(currentRenderer):
     else:
         return False 
 
-def copyLightAttributes(light,lightsData,light_type,number,convNumber):
-    try:
-        a_intensity = lightsData["Lights"][f"{light_type}"]["attribute_intensity"][number]
-        intensity=cmds.getAttr(f"{light}.{a_intensity}")
-    except:
-        pass
-    try:
-        a_color = lightsData["Lights"][f"{light_type}"]["attribute_color"][number]
-        l_color = cmds.getAttr(f"{light}.{a_color}")
-    except:
-        pass
-    try:
-        a_exposure = lightsData["Lights"][f"{light_type}"]["attribute_exposure"][number]
-        exposure = cmds.getAttr(f"{light}.{a_exposure}")
-    except:
-        pass
-    convLight=lightsData["Lights"][light_type]["name"][convNumber]
-    print(convLight)
-    convLight=cmds.shadingNode(f"{convLight}",asLight=True,name=f"{light}_conv")
-    try:
-        a_intensity = lightsData["Lights"][f"{light_type}"]["attribute_intensity"][convNumber]
-        cmds.setAttr(f"{convLight}.{a_intensity}", intensity)
-    except:
-        pass
-    try:
-        a_color = lightsData["Lights"][f"{light_type}"]["attribute_color"][convNumber]
-        cmds.setAttr(f"{convLight}.{a_color}", l_color[0][0], l_color[0][1], l_color[0][2], type="double3")
-    except:
-        pass
-    try:
-        a_exposure = lightsData["Lights"][f"{light_type}"]["attribute_exposure"][convNumber] 
-        cmds.setAttr(f"{convLight}.{a_exposure}", exposure)
-    except:
-        pass
-    lightTransform=cmds.listRelatives(light,parent=True,shapes=True,fullPath=True)
-    cmds.matchTransform(f"{convLight}", lightTransform)
-    lightSet=cmds.listRelatives(lightTransform,parent=True,fullPath=True)
-    if lightSet:
-        cmds.parent(f"{convLight}", lightSet)    
 
-def lightsConversion(convNumber):
+def materialsConversion(convNumber):
     with open('/transfer/s5512613_SP/Masters_Project/Render_Scene_Swapper/Dictionary/materials.json','r') as file :
-        lightsData=json.load(file)
+        materialsData=json.load(file)
     currentRenderer=getCurrentRenderer()    
     number=assignNumber(currentRenderer)
     sel = cmds.ls(sl=True)
@@ -77,47 +38,39 @@ def lightsConversion(convNumber):
         cmds.confirmDialog(title='Error', message='Please select an object', button=['OK'], defaultButton='OK')
         cmds.warning("Error : Please select an object")
         return
-    for obj in sel:    
-        lights = cmds.ls(sl = True, dag = True, s = True)
-        print(lights)
-        for light in lights:
-            print(light)  
-            light_type = cmds.nodeType(light)
-            print(light_type)
-            if (light_type == lightsData["Lights"]["area_light"]["name"][number]):
-                light_type="area_light"
-                print(light_type)
-                copyLightAttributes(light,lightsData,light_type,number,convNumber)
-            elif(light_type == lightsData["Lights"]["point_light"]["name"][number]):
-                light_type="point_light"
-                print(light_type)
-                copyLightAttributes(light,lightsData,light_type,number,convNumber)
-            elif(light_type == lightsData["Lights"]["directional_light"]["name"][number]):
-                light_type="directional_light"
-                print(light_type)
-                copyLightAttributes(light,lightsData,light_type,number,convNumber)
-            elif(light_type == lightsData["Lights"]["spot_light"]["name"][number]):
-                light_type="spot_light"
-                print(light_type)
-                copyLightAttributes(light,lightsData,light_type,number,convNumber)
-            elif(light_type == lightsData["Lights"]["skyDome_light"]["name"][number]):
-                light_type="skyDome_light"
-                print(light_type)
-                copyLightAttributes(light,lightsData,light_type,number,convNumber)
-            elif(light_type == lightsData["Lights"]["mesh_light"]["name"][number]):
-                light_type="mesh_light"
-                print(light_type)
-                copyLightAttributes(light,lightsData,light_type,number,convNumber) 
-            else:
-                pass
+
+    material_conversion_map = {
+        materialsData["Materials"]["standard_material"]["name"][number] : "standard_material",
+        materialsData["Materials"]["hair_material"]["name"][number] : "hair_material"
+    }
+
+    for obj in sel:
+        objects=cmds.ls(sl=True,dag=True,s=True)
+        print (objects)
+        for object in objects:
+            print (object)
+            shadeEng=cmds.listConnections(object,type='shadingEngine')
+            print(shadeEng)
+        if shadeEng :
+            for sg in shadeEng :
+                material=cmds.ls(cmds.listConnections(shadeEng),materials = True)
+                print(material)
+                if cmds.nodeType(material) != 'aiStandardSurface' : #change to read from all of json
+                    cmds.confirmDialog(title='Error', message='Selected object does not have an Arnold Standard Surface material applied.', button=['OK'], defaultButton='OK')
+                    cmds.warning("Error : Selected object does not have an Arnold Standard Surface material applied.")
+                    return
+                if material:
+                    new_material=cmds.shadingNode('PxrSurface',asShader=True) #change to get from json
+                    new_sg=cmds.sets(new_material,renderable=True, noSurfaceShader=True, empty=True, name=new_material + '_conv')
+                    cmds.connectAttr(new_material + '.outColor',new_sg + '.surfaceShader',force=True)
 
 
-def convertLights(convNumber):
+def convertMaterials(convNumber):
     currentRenderer=getCurrentRenderer()
     print(currentRenderer)
     check1=checkCurrentRenderer(currentRenderer)
     if check1== "Renderer Supported":
-        lightsConversion(convNumber)
+        materialsConversion(convNumber)
     else:
         print("Renderer not supported for conversion")
 
@@ -131,22 +84,22 @@ def createUI():
         print(f"{selected_button_f=}")
         if selected_button_f == "Arnold to Renderman":
             convNumber=1
-            convertLights(convNumber)
+            convertMaterials(convNumber)
         elif selected_button_f == "Renderman to Arnold":
             convNumber=0
-            convertLights(convNumber)
+            convertMaterials(convNumber)
         elif selected_button_f == "Arnold to VRay":
             convNumber=2
-            convertLights(convNumber)
+            convertMaterials(convNumber)
         elif selected_button_f == "VRay to Arnold":
             convNumber=0
-            convertLights(convNumber)
+            convertMaterials(convNumber)
         elif selected_button_f == "Renderman to VRay":
             convNumber=2
-            convertLights(convNumber)
+            convertMaterials(convNumber)
         elif selected_button_f == "VRay to Renderman":
             convNumber=1
-            convertLights(convNumber)
+            convertMaterials(convNumber)
     
     window_name = "Renderer_Scene_Swapper"
     if cmds.window(window_name, exists=True):
